9040c91ab352d86237ee2b43b1561ccdb5a885aa
MDBA <artes.mike@gmail.com>
2019-11-16 13:32:33 -0500

    adding on click event and variables for input boxes

---
+++ Assets/JS/script.js
@@ -0,0 +1,7 @@
+$("#search").on("click", function(){
+    var searchInput = $("#searchInput").val().trim();
+    var numberInput =$("#numberInput").val().trim();
+    var startYearInput =$("#searchYearInput").val().trim();
+    var endYearInput =$("#endYearInput").val().trim();
+    $(inp)
+})
